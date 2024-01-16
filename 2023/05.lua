55804b1fe75947dac976f2c1fddb53f2f0a98996
sroccaserra <sroccaserra@yahoo.com>
2024-01-16 20:52:31 +0100

    f05 Solve part two (Lua)

--- 2023/05.lua
+++ 2023/05.lua
@@ -1,4 +1,6 @@
-local insert = table.insert
+local push = table.insert
+local pop = table.remove
+local unpack = table.unpack
 
 local function min(values)
   local result = 9999999999999
@@ -11,25 +13,69 @@ local function min(values)
 end
 
 local function solve_1(values, maps)
-  for _, ranges in ipairs(maps) do
+  for _, map in ipairs(maps) do
     local new_values = {}
     for _, value in ipairs(values) do
-      for _, range in ipairs(ranges) do
-        local src_start = range[2]
-        local src_end = range[2] + range[3]
-        if src_start <= value and value < src_end then
-          local inc = range[1] - src_start
-          insert(new_values, value + inc)
+      for _, map_range in ipairs(map) do
+        local dst_start, src_start, n = unpack(map_range)
+        if src_start <= value and value < src_start + n then
+          local inc = dst_start - src_start
+          push(new_values, value + inc)
           goto continue
         end
       end
-      insert(new_values, value)
+      push(new_values, value)
       ::continue::
     end
     values = new_values
   end
 
   return min(values)
+end
+
+local function solve_2(values, maps)
+  -- build value ranges from input
+  local value_ranges = {}
+  for i=1,#values,2 do
+    local start_val = values[i]
+    local end_val = start_val + values[i+1]
+    push(value_ranges, {start_val, end_val})
+  end
+
+  -- see checks on inputs in python version for why this works
+  for _, map in ipairs(maps) do
+    local new_value_ranges = {}
+    while #value_ranges > 0 do
+      local value_range = pop(value_ranges)
+      local value_start, value_end = unpack(value_range)
+      for _, map_range in ipairs(map) do
+        local dst_start, src_start, n = unpack(map_range)
+        local src_end = src_start + n
+        local overlap_start = math.max(src_start, value_start)
+        local overlap_end = math.min(src_end, value_end)
+        if overlap_start < overlap_end then
+          local inc = dst_start - src_start
+          push(new_value_ranges, {overlap_start+inc, overlap_end+inc})
+          if value_start < overlap_start then
+            push(value_ranges, {value_start, overlap_start})
+          end
+          if overlap_end < value_end then
+            push(value_ranges, {overlap_end, value_end})
+          end
+          goto continue
+        end
+      end
+      push(new_value_ranges, value_range)
+      ::continue::
+    end
+    value_ranges = new_value_ranges
+  end
+
+  local mins = {}
+  for _, range in ipairs(value_ranges) do
+    push(mins, range[1])
+  end
+  return min(mins)
 end
 
 local ex = [[seeds: 79 14 55 13
@@ -71,12 +117,12 @@ local text = ex
 -- text = io.input('05'):read('*all')
 local lines = {}
 for line in text:gmatch('([^\n]*)\n?') do
-  insert(lines, line)
+  push(lines, line)
 end
 
 local seeds = {}
-for w in lines[1]:gmatch('%S+') do
-  insert(seeds, tonumber(w))
+for word in lines[1]:gmatch('%S+') do
+  push(seeds, tonumber(word))
 end
 
 local maps = {}
@@ -85,7 +131,7 @@ for i=2,#lines do
   local line = lines[i]
   if line == '' then
     ranges = {}
-    insert(maps, ranges)
+    push(maps, ranges)
     goto continue
   end
   local last = line:sub(#line)
@@ -93,11 +139,12 @@ for i=2,#lines do
     goto continue
   end
   local range = {}
-  for w in line:gmatch('%S+') do
-    insert(range, tonumber(w))
+  for word in line:gmatch('%S+') do
+    push(range, tonumber(word))
   end
-  insert(ranges, range)
+  push(ranges, range)
   ::continue::
 end
 
 print(solve_1(seeds, maps))
+print(solve_2(seeds, maps))
