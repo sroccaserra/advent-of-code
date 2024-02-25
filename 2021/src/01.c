bd3f07a46748ece95e43afa9954399dfae7ab3c2
sroccaserra <sroccaserra@yahoo.com>
2024-02-25 23:12:43 +0100

    r Use getlines function in 2021-12-01 (C)

--- 2021/src/01.c
+++ 2021/src/01.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-#include "common/IntVector.h"
+#include "common/common.h"
 
 #define HUGE 9999999
 
-int solve_1(IntVector* numbers) {
+int solve_1(int numbers[], size_t size) {
     int previous = HUGE;
     int result = 0;
-    for (size_t i = 0; i < numbers->size; ++i) {
-        int n = getInt(numbers, i);
+    for (size_t i = 0; i < size; ++i) {
+        int n = numbers[i];
         if (n > previous) {
             ++result;
         }
@@ -17,11 +17,11 @@ int solve_1(IntVector* numbers) {
     return result;
 }
 
-int solve_2(IntVector* numbers) {
+int solve_2(int numbers[], size_t size) {
     int p_1 = HUGE, p_2 = HUGE, p_3 = HUGE;
     int result = 0;
-    for (size_t i = 0; i < numbers->size; ++i) {
-        int n = getInt(numbers, i);
+    for (size_t i = 0; i < size; ++i) {
+        int n = numbers[i];
         if (n + p_1 + p_2 > p_1 + p_2 + p_3) {
             ++result;
         }
@@ -33,22 +33,24 @@ int solve_2(IntVector* numbers) {
 }
 
 int main() {
-    FILE *file = fopen("src/01.in", "r");
-    if (NULL == file) {
-        printf("Error: file not found.\n");
-        return 1;
-    }
+    size_t size;
+    char** lines;
+    int res = getlines("src/01.in", &lines, &size);
+    assert(0 == res);
 
-    IntVector numbers = createIntVector();
-    int n;
-    while(EOF != fscanf(file, "%d\n", &n)) {
-        pushInt(&numbers, n);
+    int numbers[size];
+    for (size_t i = 0; i < size; ++i) {
+        char* line = lines[i];
+        sscanf(line, "%d\n", &numbers[i]);
+        free(lines[i]);
+        lines[i] = NULL;
     }
-    fclose(file);
+    free(lines);
+    lines = NULL;
 
     printf("%d\n%d\n",
-            solve_1(&numbers),
-            solve_2(&numbers));
+            solve_1(numbers, size),
+            solve_2(numbers, size));
 
     return 0;
 }
