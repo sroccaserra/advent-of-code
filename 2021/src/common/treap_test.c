347ac2842b228f653a064400014b80421b7d1b80
sroccaserra <sroccaserra@yahoo.com>
2024-03-26 15:56:32 +0100

    r Move treap tests to test file

---
+++ 2021/src/common/treap_test.c
@@ -0,0 +1,208 @@
+#include <assert.h>
+#include <error.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "treap.h"
+
+/**************
+ * Test tools *
+ **************/
+
+static void aei(int expected, int actual) {
+    if (expected != actual) {
+        error(1, 0, "ASSERTION FAILED!\n⭕ %d\n❌ %d", expected, actual);
+    }
+}
+
+static void aes(char* expected, char* actual) {
+    if (NULL == expected) {
+        expected = "(NULL)";
+    }
+    if (NULL == actual) {
+        actual = "(NULL)";
+    }
+    if (0 != strcmp(expected, actual)) {
+        error(1, 0, "ASSERTION FAILED!\n⭕\n%s\n❌\n%s", expected, actual);
+    }
+}
+
+#define BUF_SIZE 4096
+
+extern FILE* fmemopen(void* buf, size_t size, const char* mode);
+
+static void tsprint(char* buf, treap_t t) {
+    FILE* f = fmemopen(buf, BUF_SIZE, "w");
+    tfprint(f, t);
+    fclose(f);
+}
+
+struct entry {
+    char* key;
+    double priority;
+    void* value;
+};
+
+static void build_treap(struct entry* entries, treap_t t) {
+    size_t i = 0;
+    struct entry e;
+    while ((e = entries[i++]).key != NULL) {
+        treap_insert(t, e.key, e.value, e.priority);
+    }
+}
+
+/*********
+ * Tests *
+ *********/
+
+static void test_treap_free_sets_pointer_to_null() {
+    treap_t t = treap_alloc();
+    treap_free(&t);
+
+    assert(t == NULL);
+}
+
+static void test_insert_exemple_from_book() {
+    struct entry entries[] = {
+        {"Bacon", 77, NULL},
+        {"Beer", 20, NULL},
+        {"Butter", 76, NULL},
+        {"Cabbage", 159, NULL},
+        {"Eggs", 129, NULL},
+        {"Floor", 10, NULL},
+        {"Milk", 55, NULL},
+        {"Pork", 56, NULL},
+        {"Water", 32, NULL},
+        {NULL, 0, NULL},
+    };
+
+    treap_t t = treap_alloc();
+    build_treap(entries, t);
+
+    char buf[BUF_SIZE];
+    tsprint(buf, t);
+
+    aes("^:Floor - 10.00\n"
+            "  <:Beer - 20.00\n"
+            "    <:Bacon - 77.00\n"
+            "    >:Butter - 76.00\n"
+            "      >:Eggs - 129.00\n"
+            "        <:Cabbage - 159.00\n"
+            "  >:Water - 32.00\n"
+            "    <:Milk - 55.00\n"
+            "      >:Pork - 56.00\n"
+            , buf);
+
+    treap_free(&t);
+}
+
+static void test_search() {
+    int value = 1234;
+    struct entry entries[] = {
+        {"Bacon", 77, &value},
+        {"Beer", 20, NULL},
+        {"Butter", 76, NULL},
+        {"Cabbage", 159, NULL},
+        {"Eggs", 129, NULL},
+        {"Floor", 10, NULL},
+        {"Milk", 55, NULL},
+        {"Pork", 56, NULL},
+        {"Water", 32, NULL},
+        {NULL, 0, NULL},
+    };
+    treap_t t = treap_alloc();
+    build_treap(entries, t);
+
+    int* result = treap_search(t, "Bacon");
+    assert(1234 == *result);
+
+    result = treap_search(t, "dontexist");
+    assert(NULL == result);
+
+    treap_free(&t);
+}
+
+static void test_remove() {
+    struct entry entries[] = {
+        {"Beer", 20, NULL},
+        {"Beet", 81, NULL},
+        {"Butter", 76, NULL},
+        {"Cabbage", 159, NULL},
+        {"Eggs", 129, NULL},
+        {"Floor", 10, NULL},
+        {"Milk", 55, NULL},
+        {"Pork", 56, NULL},
+        {"Water", 32, NULL},
+        {NULL, 0, NULL},
+    };
+
+    treap_t t = treap_alloc();
+    build_treap(entries, t);
+
+    char buf[BUF_SIZE];
+    tsprint(buf, t);
+
+    aes("^:Floor - 10.00\n"
+            "  <:Beer - 20.00\n"
+            "    >:Butter - 76.00\n"
+            "      <:Beet - 81.00\n"
+            "      >:Eggs - 129.00\n"
+            "        <:Cabbage - 159.00\n"
+            "  >:Water - 32.00\n"
+            "    <:Milk - 55.00\n"
+            "      >:Pork - 56.00\n"
+            , buf);
+
+    bool result = treap_remove(t, "Butter");
+
+    assert(result);
+    tsprint(buf, t);
+    aes("^:Floor - 10.00\n"
+            "  <:Beer - 20.00\n"
+            "    >:Beet - 81.00\n"
+            "      >:Eggs - 129.00\n"
+            "        <:Cabbage - 159.00\n"
+            "  >:Water - 32.00\n"
+            "    <:Milk - 55.00\n"
+            "      >:Pork - 56.00\n"
+            , buf);
+
+
+    treap_free(&t);
+}
+
+static void test_size() {
+    struct entry entries[] = {
+        {"Beer", 20, NULL},
+        {"Beet", 81, NULL},
+        {"Butter", 76, NULL},
+        {"Cabbage", 159, NULL},
+        {"Eggs", 129, NULL},
+        {"Floor", 10, NULL},
+        {"Milk", 55, NULL},
+        {"Pork", 56, NULL},
+        {"Water", 32, NULL},
+        {NULL, 0, NULL},
+    };
+
+    treap_t t = treap_alloc();
+    build_treap(entries, t);
+
+    size_t result = treap_size(t);
+    aei(9, result);
+
+    treap_remove(t, "Butter");
+    result = treap_size(t);
+    aei(8, result);
+
+    treap_free(&t);
+}
+
+int main() {
+    test_treap_free_sets_pointer_to_null();
+    test_insert_exemple_from_book();
+    test_search();
+    test_remove();
+    test_size();
+    return 0;
+}
