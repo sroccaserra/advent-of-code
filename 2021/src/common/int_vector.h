400a87740848ba284ddf7657550a7acd4c36abb2
sroccaserra <sroccaserra@yahoo.com>
2024-03-29 09:36:07 +0100

    r Rename int_vector (make it like other C types)

--- 2021/src/common/int_vector.h
+++ 2021/src/common/int_vector.h
@@ -1,24 +1,33 @@
-#ifndef __INT_VECTOR_H__
-#define __INT_VECTOR_H__
+#ifndef INT_VECTOR_H
+#define INT_VECTOR_H
 
 #include <stdlib.h>
 #include <assert.h>
 
-typedef struct {
+#define V int_vector_t
+typedef struct V* V;
+
+struct V {
     int* data;
     size_t size;
     size_t max_size;
-} IntVector;
+};
 
 const size_t default_size = 8;
 
-IntVector createIntVector() {
+V int_vector_alloc() {
     int* data = malloc(sizeof(int[default_size]));
     assert(NULL != data);
-    return (IntVector){.data = data, .size = 0, .max_size = default_size};
+
+    V result = malloc(sizeof(*result));
+    result->data = data;
+    result->size = 0;
+    result->max_size = default_size;
+    return result;
 }
 
-void deleteIntVector(IntVector* v) {
+void int_vector_free(V* vp) {
+    V v = *vp;
     assert(NULL != v);
     assert(NULL != v->data);
 
@@ -26,9 +35,10 @@ void deleteIntVector(IntVector* v) {
     v->data = NULL;
     v->size = 0;
     v->max_size = default_size;
+    *vp = NULL;
 }
 
-void resizeIntVector(IntVector* v, size_t new_max_size) {
+static void int_vector_resize(V v, size_t new_max_size) {
     assert(NULL != v);
     assert(NULL != v->data);
     assert(v->max_size < new_max_size);
@@ -40,24 +50,25 @@ void resizeIntVector(IntVector* v, size_t new_max_size) {
     v->max_size = new_max_size;
 }
 
-int getInt(IntVector* v, size_t pos) {
+int int_vector_get(V v, size_t pos) {
     assert(pos < v->size);
     return v->data[pos];
 }
 
-void setInt(IntVector* v, size_t pos, int value) {
+void int_vector_set(V v, size_t pos, int value) {
     assert(pos < v->size);
     v->data[pos] = value;
 }
 
-void pushInt(IntVector* v, int value) {
+void int_vector_push(V v, int value) {
     size_t new_pos = v->size;
     if (v->max_size <= new_pos) {
-        resizeIntVector(v, 2*v->max_size);
+        int_vector_resize(v, 2*v->max_size);
     }
     assert (new_pos < v->max_size);
     v->data[new_pos] =value;
     v->size += 1;
 }
 
-#endif // __INT_VECTOR_H__
+#undef V
+#endif
