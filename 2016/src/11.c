2ace9087e3e4520cb0b35485dc74df113ea29e6d
sroccaserra <sroccaserra@yahoo.com>
2024-05-30 18:26:31 +0200

    r Regroup state initialization

--- 2016/src/11.c
+++ 2016/src/11.c
@@ -92,27 +92,6 @@ bool is_at_floor(struct state *state, int element_id, int floor_number) {
     return floor_number == get_position(state, element_id);
 }
 
-struct state build_state(struct element **elements_by_floor) {
-    for (size_t i = 0; i < NB_FLOORS; ++i) {
-        struct element *elements_at_floor = elements_by_floor[i];
-        for (size_t j = 0; j < da_size(elements_at_floor); ++j) {
-            elements[nb_elements++] = elements_at_floor[j];
-        }
-    }
-    qsort(elements, nb_elements, sizeof elements[0], cmp_elements);
-
-    struct state result = {0};
-    for (size_t floor_number = 0; floor_number < NB_FLOORS; ++floor_number) {
-        struct element *floor = elements_by_floor[floor_number];
-        result.floors[floor_number] = 0;
-        for (size_t j = 0; j < da_size(floor); ++j) {
-            int element_id = find_element_id(floor[j]);
-            set_position(&result, element_id, floor_number);
-        }
-    }
-    return result;
-}
-
 void print_floor(struct state *state, int floor_number) {
     printf("F%d ",floor_number+1);
     for (size_t i = 0; i < nb_elements; ++i) {
@@ -151,9 +130,9 @@ uint64_t solve_1(struct state state) {
     return *(uint64_t*)state.floors;
 }
 
-/*************************
- * Parsing & entry point *
- *************************/
+/***********
+ * Parsing *
+ ***********/
 
 struct element *parse_line(char *line) {
     char **words = split(line, " ,.");
@@ -197,6 +176,49 @@ end:
     return result;
 }
 
+void parse_input(char* lines[], struct element *elems_by_floor[]) {
+    nb_elements = 0;
+    size_t nb_lines = da_size(lines);
+    for (size_t i = 0; i < nb_lines; ++i) {
+        char *line = lines[i];
+        struct element *elements_at_floor = parse_line(line);
+        elems_by_floor[i] = elements_at_floor;
+    }
+    assert(nb_elements <= MAX_ELEMENTS);
+}
+
+void init(char* lines[], struct state *state) {
+    // Parsing lines as floors
+    struct element *elems_by_floor[NB_FLOORS];
+    parse_input(lines, elems_by_floor);
+
+    // Building an ordered list of elements
+    for (size_t i = 0; i < NB_FLOORS; ++i) {
+        struct element *elements_at_floor = elems_by_floor[i];
+        for (size_t j = 0; j < da_size(elements_at_floor); ++j) {
+            elements[nb_elements++] = elements_at_floor[j];
+        }
+    }
+    qsort(elements, nb_elements, sizeof elements[0], cmp_elements);
+
+    // Using the above to build a state
+    *state = (struct state){0};
+    for (size_t floor_number = 0; floor_number < NB_FLOORS; ++floor_number) {
+        struct element *floor = elems_by_floor[floor_number];
+        state->floors[floor_number] = 0;
+        for (size_t j = 0; j < da_size(floor); ++j) {
+            int element_id = find_element_id(floor[j]);
+            set_position(state, element_id, floor_number);
+        }
+    }
+
+    // free elements by floor arrays
+    for (size_t i = 0; i < NB_FLOORS; ++i) {
+        da_free(elems_by_floor[i]);
+    }
+}
+
+
 /*********
  * Tests *
  *********/
@@ -218,17 +240,6 @@ void test(void) {
     assert(get_position(&state, 0) == 3);
     assert(state.floors[3] == 0x001);
     assert(state.floors[1] == 0x000);
-}
-
-void parse_input(char* lines[], struct element *elems_by_floor[]) {
-    nb_elements = 0;
-    size_t nb_lines = da_size(lines);
-    for (size_t i = 0; i < nb_lines; ++i) {
-        char *line = lines[i];
-        struct element *elements_at_floor = parse_line(line);
-        elems_by_floor[i] = elements_at_floor;
-    }
-    assert(nb_elements <= MAX_ELEMENTS);
 }
 
 /*
@@ -260,18 +271,13 @@ int main(int argc, char **argv) {
     size_t nb_lines = da_size(lines);
     assert(NB_FLOORS == nb_lines);
 
-    struct element *elems_by_floor[NB_FLOORS];
-    parse_input(lines, elems_by_floor);
+    struct state state;
+    init(lines, &state);
+
     for (size_t i = 0; i < nb_lines; ++i) {
         free(lines[i]);
     }
     da_free(lines);
-
-    struct state state = build_state(elems_by_floor);
-
-    for (size_t i = 0; i < NB_FLOORS; ++i) {
-        da_free(elems_by_floor[i]);
-    }
 
     if (NULL != getenv("TEST")) {
         printf("Testing 11...");
