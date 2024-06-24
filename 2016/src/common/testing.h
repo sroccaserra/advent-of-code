c2f8a48d1c9fad263e90e15cfb635d8b5deae250
sroccaserra <sroccaserra@yahoo.com>
2024-06-24 10:12:30 +0200

    r Flush stdout on test start

--- 2016/src/common/testing.h
+++ 2016/src/common/testing.h
@@ -5,7 +5,7 @@
 #include <assert.h>
 #include <string.h>
 
-#define TEST_START(s) (printf("Testing "s"..."))
+#define TEST_START(s) (printf("Testing "s"..."), fflush(stdout))
 #define TEST_END (printf(" ✅ OK.\n"))
 
 int equals_int(int expected, int actual) {
