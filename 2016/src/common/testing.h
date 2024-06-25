6fea2c31de56a8ed8d0d12ef0d2bb50ee3771723
sroccaserra <sroccaserra@yahoo.com>
2024-06-25 11:00:54 +0200

    r Move towards a more extensible assert_equals

--- 2016/src/common/testing.h
+++ 2016/src/common/testing.h
@@ -1,49 +1,60 @@
 #ifndef TESTING_H
 #define TESTING_H
 
-#include <stdio.h>
 #include <assert.h>
-#include <string.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 #define TEST_START(s) (printf("Testing "s"..."), fflush(stdout))
 #define TEST_END (printf(" ✅ OK.\n"))
 
-int equals_uint64_t(uint64_t expected, uint64_t actual) {
-    if (expected != actual) {
-        fprintf(stderr, "ASSERTION FAILED!\n👉 %ld\n❌ %ld\n", expected, actual);
-        return 0;
-    }
-    return 1;
+#define gen_format(e) _Generic((e), \
+        uint64_t: "%lu", \
+        int64_t: "%ld", \
+        int: "%d", \
+        void *: "%p", \
+        char *: "%s") \
+
+#define gen_print_failure(e, a) ( \
+        fprintf(stderr, "ASSERTION FAILED!\n👉 "), \
+        fprintf(stderr, gen_format(e), e), \
+        fprintf(stderr, "\n❌ "), \
+        fprintf(stderr, gen_format(a), a), \
+        fprintf(stderr, "\n"))
+
+#define check_equals(e, a) ((e) == (a) ? 1 : ((gen_print_failure(e, a)), 0))
+
+int check_equals_uint64_t(uint64_t expected, uint64_t actual) {
+    return check_equals(expected, actual);
 }
 
-int equals_int(int expected, int actual) {
-    if (expected != actual) {
-        fprintf(stderr, "ASSERTION FAILED!\n👉 %d\n❌ %d\n", expected, actual);
-        return 0;
-    }
-    return 1;
+int check_equals_int(int expected, int actual) {
+    return check_equals(expected, actual);
 }
 
-int equals_str(char *expected, char *actual) {
+int check_equals_ptr(void *expected, void *actual) {
+    return check_equals(expected, actual);
+}
+
+int check_equals_str(char *expected, char *actual) {
     if (0 != strcmp(expected, actual)) {
-        fprintf(stderr, "ASSERTION FAILED!\n👉 %s\n❌ %s\n", expected, actual);
+        gen_print_failure(expected, actual);
         return 0;
     }
     return 1;
 }
 
-#define ae_eq(e, a) _Generic((e), uint64_t: equals_uint64_t, int: equals_int, char *: equals_str)(e, a)
-#define assert_equals(e, a) (assert(ae_eq(e, a)))
+#define gen_check_equals(e, a) _Generic((e), \
+        uint64_t: check_equals_uint64_t, \
+        int: check_equals_int, \
+        void *: check_equals_ptr, \
+        char *: check_equals_str) \
+    (e, a)
 
-int check_null(void* p) {
-    if (NULL != p) {
-        fprintf(stderr, "ASSERTION FAILED!\n👉 %p\n❌ %p\n", NULL, p);
-        return 0;
-    }
-    return 1;
-}
 
-#define assert_null(p) (assert(check_null(p)))
+#define assert_equals(e, a) (assert(gen_check_equals(e, a)))
+
+#define assert_null(p) (assert(gen_check_equals(NULL, p)))
 
 #endif
