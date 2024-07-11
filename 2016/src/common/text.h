221f9bd822f11ed536684ae19ae5872a524058a9
sroccaserra <sroccaserra@yahoo.com>
2024-07-11 15:57:08 +0200

    r Use split() in split_lines()

--- 2016/src/common/text.h
+++ 2016/src/common/text.h
@@ -40,7 +40,6 @@ long slurp(struct arena *a, char *filename, char **ptext) {
 
 /********************************************************
  * Split                                                *
- *                                                      *
  * Note: this function is destructive, like strtok_r(). *
  ********************************************************/
 
@@ -83,44 +82,13 @@ int split(struct arena *a, char *text, const char *sep, char **items[]) {
     return nb_items;
 }
 
-/***********************************************************
- * Split Lines                                             *
- *                                                         *
- * Note: this function is destructive, because strtok_r(). *
- ***********************************************************/
+/********************************************************
+ * Split Lines                                          *
+ * Note: this function is destructive, like strtok_r(). *
+ ********************************************************/
 
 int split_lines(struct arena *a, char *text, char **lines[]) {
-    if ('\0' == text[0]) {
-        *lines = NULL;
-        return 0;
-    }
-
-    char **items;
-    size_t item_size = sizeof(items[0]);
-    int capacity = 8;
-    items = arena_push(a, capacity*item_size);
-
-    char *save_ptr = NULL;
-    int nb_items = 0;
-    while (1) {
-        if (capacity < nb_items) {
-            arena_push(a, capacity*item_size); // extend the allocated size for items
-            capacity += capacity;
-        }
-        char *token = strtok_r(text, "\n", &save_ptr);
-        items[nb_items] = token;
-        if (NULL == token) {
-            break;
-        }
-        ++nb_items;
-        text = NULL;
-    }
-
-    size_t excess = (capacity - nb_items)*item_size;
-    arena_pop(a, excess); // trim the allocated size for items
-
-    *lines = items;
-    return nb_items;
+    return split(a, text, "\n", lines);
 }
 
 /*************
