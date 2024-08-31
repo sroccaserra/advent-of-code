3cc3dc5558ee650c60d393b985fabb9469619b78
sroccaserra <sroccaserra@yahoo.com>
2024-08-31 12:10:48 +0200

    r Hide struct arena details

--- 2016/src/common/arena.c
+++ 2016/src/common/arena.c
@@ -3,6 +3,12 @@
 #include <assert.h>
 #include <stdlib.h>
 
+struct arena {
+    size_t capacity;
+    size_t used;
+    char *mem;
+};
+
 struct arena *arena_init(size_t capacity) {
     assert(capacity);
     char *mem = malloc(capacity);
@@ -17,6 +23,14 @@ void arena_discard(struct arena *a) {
     free(a->mem);
     *a = (struct arena){0};
     free(a);
+}
+
+#define arena_pointer(a) ((void *)((a)->mem + (a)->used))
+
+void *arena_top(struct arena *a) {
+    void *result = arena_pointer(a);
+
+    return result;
 }
 
 void *arena_push(struct arena *a, size_t size) {
